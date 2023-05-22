a461466cf74a43a066e37703ca01431bddea42a8
Ryhor <ryhor,dubrovin@pragmaticplay.com>
2023-05-22 22:18:19 +0400

    drap and drop tests

--- src/test/java/pages/DragAndDropPage.java
+++ src/test/java/pages/DragAndDropPage.java
@@ -3,20 +3,37 @@ package pages;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 
 import static constants.Urls.DRAG_AND_DROP_PAGE;
 
 public class DragAndDropPage extends BasePage {
-    private By IFRAME = By.xpath("//iframe[@class='demo-frame']");
-
-//    WebElement iframe = driver.findElement(IFRAME);
-//    driver.switchTo().frame(iframe);
+    @FindBy(className = "demo-frame")
+    private WebElement frame;
+    @FindBy(id = "draggable")
+    private WebElement draggable;
+    @FindBy(id = "droppable")
+    private WebElement droppable;
+    private final String successMessage = "Dropped!";
+    public String getSuccessMessage() {
+        return getSuccessMessage();
+    }
+    public String getDroppableMessage() {
+        driver.switchTo().frame(frame);
+        return driver.findElement(By.xpath("//*[@id='droppable']/p")).getText();
+        //return droppable.getText();
+    }
 
     public DragAndDropPage(WebDriver driver) {
         super(driver);
     }
-
-    public void openCheckboxesPage() {
+    public void openDragAndDropPage() {
         driver.get(DRAG_AND_DROP_PAGE);
+    }
+    public void dragAndDrop() {
+        driver.switchTo().frame(frame);
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(draggable,droppable).build().perform();
     }
 }
--- src/test/java/tests/DragAndDropTest.java
+++ src/test/java/tests/DragAndDropTest.java
@@ -1,7 +1,19 @@
 package tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import pages.DragAndDropPage;
 
 public class DragAndDropTest extends BaseTest {
+    @Test
+    public void dragAndDropTest() {
+        DragAndDropPage dragAndDropPage = new DragAndDropPage(driver);
+        dragAndDropPage.openDragAndDropPage();
+        dragAndDropPage.dragAndDrop();
+        System.out.println(dragAndDropPage.getDroppableMessage());
+        Assert.assertEquals(dragAndDropPage.getDroppableMessage(),
+                dragAndDropPage.getSuccessMessage(),
+                "Element not dropped");
+    }
 
 }
